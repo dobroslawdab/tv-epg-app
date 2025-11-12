414f43a7adbfdb1a2fe98acbc685e961db92ffd9
dobroslawdab <dobroslawdab@users.noreply.github.com>
2025-11-12 20:09:31 +0100

    chore: Bump version to 3.5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
           applicationId = "com.uxellence.tv.v3"
           minSdk = 21
           targetSdk = 35
-          versionCode = 23
-          versionName = "3.5.5"
+          versionCode = 24
+          versionName = "3.5.6"
   
           // Specify the ABIs to build for. Including both 32-bit and 64-bit ensures compatibility.
           ndk {
